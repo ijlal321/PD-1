966d92845bcc5039e9e1dd1fd1e0190bb5dbc68b
ijlal321 <103317626+ijlal321@users.noreply.github.com>
2022-12-27 23:54:08 +0500

    Add files via upload

---
+++ week 1/lab 1/file7.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+cout << "          	     	               "   << endl;
+cout << "          ______     ~    ______      "   << endl;
+cout << "          \     \__/ ^ \__/   /       "   << endl;
+cout << "           \		      /        "   << endl;
+cout << "            )____	 ____(         "   << endl;
+cout << "             	  )	(              "   << endl;
+cout << "          	   \ _ /               "   << endl;
+cout << "          	     	               "   << endl;
+
+
+
+
+
+}
