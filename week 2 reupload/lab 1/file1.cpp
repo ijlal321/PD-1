ef98c13e56196bc779cc36f3d8dcdc18159b8b5c
ijlal321 <103317626+ijlal321@users.noreply.github.com>
2023-01-28 19:23:29 +0500

    week2
    week 2

---
+++ week 2 reupload/lab 1/file1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+cout << "_______________________"<< endl;
+cout << "|                      |"<< endl;
+cout << "|                      |"<< endl;
+cout << "|                      |"<< endl;
+cout << "|                      |"<< endl;
+cout << "|                      |"<< endl;
+cout << "_______________________"<< endl;
+}
---
+++ week 2 reupload/lab 1/task3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+cout << "      __________________ " << endl;
+cout << "     /                  / " << endl;
+cout << "    /                  / " << endl;
+cout << "   /                  / " << endl;
+cout << "  /                  / " << endl;
+cout << " /                  / " << endl;
+cout << "/__________________/ " << endl;
+
+
+
+
+}
